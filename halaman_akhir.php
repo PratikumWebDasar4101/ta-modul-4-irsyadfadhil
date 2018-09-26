30d868751b6cf3986a268ba1e9d86df219ffed90
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-26 20:54:13 +0700

    Update halaman_akhir.php

--- halaman_akhir.php
+++ halaman_akhir.php
@@ -1,1 +1,69 @@
-halaman akhir bisa diperlukan
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <H1><center>HASIL FORM ANDA</center></H1><br>
+    <hr>
+    <table align="center" border='2'>
+        <tr>
+            <th>Genre Film</th>
+            <th>Tujuan Wisata</th>
+        </tr>
+        <tr>
+            <td>
+                <?php
+                    foreach($_SESSION['film'] as $pilihan){
+                        echo $pilihan ."<br>";
+                    }
+                ?>
+            </td>
+            <td>
+                 <?php
+                 foreach($_SESSION['wisata'] as $pilihan){
+                     echo $pilihan . "<br>";
+                 }   
+                 ?>
+            </td>
+        </tr>
+        <tr> 
+            <td>
+                <a href="formawal.html">Hapus Data</a>
+            </td>
+            <td>
+                <a href="formawal.html">Hapus Data</a>
+            </td>
+        </tr>
+    </table>
+    <hr>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <a href="proses_login1.php">LOGOUT</a>
+    <hr>
+</body>
+</html>
