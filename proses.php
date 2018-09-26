11b2e5787e99c604512c154352f6ce4f7da93e84
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-26 20:54:45 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,11 @@
 <?php
+session_start();
 
+$film = $_POST['film'];
+$wisata = $_POST['wisata'];
+
+$_SESSION['film'] = $_POST['film'];
+$_SESSION['wisata'] = $_POST['wisata'];
+
+header("location:halamanakhir.php");
 ?>
