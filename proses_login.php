41912f345f8960f8030e80fb5ad059dba375c0a5
irsyadfadhil <42725274+irsyadfadhil@users.noreply.github.com>
2018-09-26 21:37:24 +0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,3 +1,43 @@
 <?php
+session_start();
+    $user = array(
+        array("ichad","ichad")
+    );
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST">
+    <H1><center>LOGIN</center></H1><br>
+    <hr>
+    Nama:<input type="text" name="name"><br><br>
+    Password :<input type="password" name="password">
+    <input type="submit" value ="login">
+    <hr>
+</form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST['name'])){
+        $username = $_POST['name'];
+        $password = $_POST['password'];
+
+        $_SESSION['name']= $username;
+
+        $cek=0;
+        for ($i=0; $i <count($user) ; $i++) { 
+            if($user[$i][0]== $username && $user[$i][1]== $password){
+                header("Location: halaman_belanja.php");
+                $cek=1;
+            }
+        }
+        if($cek==0){
+            echo "Nama/Password anda salah!";
+        }
+    }
 ?>
